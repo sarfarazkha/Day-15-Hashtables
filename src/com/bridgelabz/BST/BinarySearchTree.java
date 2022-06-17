9d823f2c8ac7013577af8cbadbb42650707b8932
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-17 23:43:30 +0530

    BST shown in figure

---
+++ src/com/bridgelabz/BST/BinarySearchTree.java
@@ -0,0 +1,84 @@
+package com.bridgelabz.BST;
+
+
+/**
+ *@PROBLEM_STATEMENT : UC2 ability to create the binary tree shown in the figure
+ * - Check if all are added with using size method in Binary Tree
+ */
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        Node left = null;
+        Node right = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Node [data=" + data + ", left=" + left + ", right=" + right + "]";
+    }
+
+}
+
+public class BinarySearchTree {
+    int size = 0;
+
+    /*
+     * @Purpose : Insert Elements in BST
+     *
+     * @Return : Node
+     */
+    public Node insert(Node node, int data) {
+
+        if (node == null) {
+            node = createNewNode(data);
+        }
+
+        if (data < node.data) {
+            node.left = insert(node.left, data);
+        } else if (data > node.data) {
+            node.right = insert(node.right, data);
+        }
+
+        return node;
+    }
+
+    /*
+     * @Purpose : Create New Node For Elements in BST
+     *
+     * @Return : MyNode
+     */
+    private Node createNewNode(int data) {
+        Node mynode = new Node(data);
+        size += 1;
+        return mynode;
+    }
+
+    /*
+     * @Purpose : Print InOrder Traversal in BST
+     *
+     * @Return : Null
+     */
+    public void printInorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        printInorder(node.left);
+        System.out.print(node.data + "->");
+        printInorder(node.right);
+
+    }
+
+    /*
+     * @Purpose : Getting Size Of BST
+     *
+     * @Return : Size
+     */
+    public int size() {
+        return size;
+    }
+}
